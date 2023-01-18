c6ef2d8e3281dfb977e065bab1fda3d59d43b083
modotte <runecroon@gmail.com>
2023-01-19 03:51:02 +0800

    Add Constant

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,16 @@
 
 module Main (main) where
 
-import Cardx.Model
+import Cardx.Constant qualified as CC
+import Cardx.Model (Card (CWild))
+import Cardx.WildCard (WildCard (..))
+import Cardx.WildKind (WildKind)
 import Data.Vector (Vector)
 import Data.Vector qualified as V
 import Relude
+
+makeWilds :: WildKind -> Vector Card
+makeWilds x = V.replicate 4 (CWild (WildCard {kind = x, score = CC.wildScore}))
 
 main :: IO ()
 main = putStrLn "Hello"
---
+++ src/Cardx/Constant.hs
@@ -0,0 +1,10 @@
+module Cardx.Constant (maxScore, actionScore, wildScore) where
+
+maxScore :: Integer
+maxScore = 500
+
+actionScore :: Integer
+wildScore :: Integer
+actionScore = 20
+
+wildScore = 50
